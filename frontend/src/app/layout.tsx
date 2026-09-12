a37f9292f7651598f54f86f68744c3074bcff4b6
toshnotik <toshnotik666@mail.ru>
2026-09-12 18:23:06 +0300

    fix: make docker setup reproducible

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,28 +1,26 @@
 import type { Metadata } from "next";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
-    title: 'Тестовое задание Fullstack',
-    description: 'Тестовое задание Fullstack',
+    title: "Тестовое задание Fullstack",
+    description: "Тестовое задание Fullstack",
   };
 }
 
 export default async function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='ru'>
+    <html lang="ru">
       <head>
-        <link rel="icon" href="/public/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body>
-        <Container fluid className='p-0'>
-            {children}
-        </Container>
+        <Container fluid className="p-0">{children}</Container>
       </body>
     </html>
   );
