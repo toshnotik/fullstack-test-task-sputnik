20ebc452bfaa0356e17390f080d6c4d9acd08959
toshnotik <toshnotik666@mail.ru>
2026-09-12 20:38:46 +0300

    fix: update Alembic database config import

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -1,17 +1,20 @@
 import asyncio
 from logging.config import fileConfig
+
+from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-from alembic import context
-from src.service import DB_URL
+
+from src.core.config import DATABASE_URL
 from src.models import Base
+
 import src.models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option('sqlalchemy.url', DB_URL)
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
